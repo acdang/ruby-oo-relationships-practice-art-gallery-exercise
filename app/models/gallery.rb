071fff65252b10c7c6d5a3436a4b65c98859cccc
Anh Dang <anhcdang@gmail.com>
2021-06-11 08:12:48 -0700

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,45 @@
 class Gallery
 
   attr_reader :name, :city
-
+  @@all = []
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select {|painting| painting.gallery == self}
+  end
+
+  def artists
+    self.paintings.map {|painting| painting.artist}
+  end
+
+  def artist_names
+    self.artists.map {|artist| artist.name}
+  end
+
+  def most_expensive_painting
+    self.paintings.max{|painting1,painting2| painting1.price <=> painting2.price}
+  end
 end
+
+# Gallery.all
+# Returns an array of all the galleries
+
+# Gallery#paintings
+# Returns an array of all paintings in a gallery
+
+# Gallery#artists
+# Returns an array of all artists that have a painting in a gallery
+
+# Gallery#artist_names
+# Returns an array of the names of all artists that have a painting in a gallery
+
+# Gallery#most_expensive_painting
+# Returns an instance of the most expensive painting in a gallery
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,18 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Picasso", 2)
+a2 = Artist.new("Mike", 3)
+a3 = Artist.new("Frida", 4)
 
+g1 = Gallery.new("SFMOMA", "San Fran")
+g2 = Gallery.new("Museum of Britist Art", "New Haven")
+g3 = Gallery.new("Louvre", "Paris")
 
+a1.create_painting("Bull", 2, g1)
+a2.create_painting("Naked Men", 10, g2)
+a3.create_painting("Joe",3,g1)
+a3.create_painting("Joe the Orange", 15, g3)
+a3.create_painting("The Orange", 100, g3)
 
 binding.pry
 
